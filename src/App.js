6271cd6cc45e571e2ffa76f8f3308c5f25ecdd9a
shiv shankar <shivssharma1990@gmail.com>
2018-09-29 12:41:15 +0530

    router setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// components
+import Login from './components/auth/login/Login'
+import AdminDashBoard from './components/admin/Dashboard'
+import StudentDashboard from './components/student/Dashboard'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Login} />
+          <Route path="/admin/dashboard" exact component={AdminDashBoard} />
+          <Route path="/student/dashboard" exact component={StudentDashboard} />
+        </div>
+      </Router>
+    )
+  }
+}
